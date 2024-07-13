8c2f67c9de986a712ca6ac75321e149bc21ed8c8
IKoshelev <koshelew@live.com>
2024-07-13 20:46:03 +0200

    Added missing value

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ public class Program
 #if DEBUG
 #else
         //dotnet run -c Release
-        //var summary1 = BenchmarkDotNet.Running.BenchmarkRunner.Run<StringPalindromeBenchmark>();
+        var summary1 = BenchmarkDotNet.Running.BenchmarkRunner.Run<StringPalindromeBenchmark>();
         var summary2 = BenchmarkDotNet.Running.BenchmarkRunner.Run<StringSequenceParseBenchmark>();
         var summary3 = BenchmarkDotNet.Running.BenchmarkRunner.Run<ByteArrayCompareWithThresholdBenchmark>();
 #endif
